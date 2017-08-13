8be902fee22bff001ef9454011ea035c0f4850f7
fandong <fand@wisenjoy.com>
2017-08-13 15:19:58 +0800

    提交Alamofire依赖

---
+++ FDTX/Module/Container/ContainerViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ContainerViewController.swift
+//  FDTX
+//
+//  Created by fandong on 2017/8/5.
+//  Copyright © 2017年 fandong. All rights reserved.
+//
+
+import UIKit
+
+class ContainViewController: UIViewController{
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
