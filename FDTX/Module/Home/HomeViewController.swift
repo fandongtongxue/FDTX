71ea236a2c21913167574cdbc1f2e44dc24c7a1b
fandong <fand@wisenjoy.com>
2017-08-29 17:40:16 +0800

    添加TableView

--- FDTX/Module/Home/HomeViewController.swift
+++ FDTX/Module/Home/HomeViewController.swift
@@ -10,20 +10,16 @@ import UIKit
 import MediaPlayer
 import MobilePlayer
 
-class HomeViewController: BaseViewController{
+let HomeViewControllerCellId = "HomeViewControllerCellId"
+let array = ["网页","Unsplash","音乐播放器","视频播放器"]
+
+class HomeViewController: BaseViewController,UITableViewDelegate,UITableViewDataSource{
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         self.view.backgroundColor = UIColor.black
         self.title = "首页"
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        super.touchesBegan(touches, with: event)
-//        self.showWebVC()
-//        self.showPictureVC()
-//        self.showMusicPlayerVC()
-        self.showVideoPlayerVC()
+        self.view.addSubview(self.tableView)
     }
     
     func showPictureVC() {
@@ -44,6 +40,53 @@ class HomeViewController: BaseViewController{
     
     func showVideoPlayerVC() {
         let playerVC = MobilePlayerViewController(contentURL: URL.init(string: "http://om2bks7xs.bkt.clouddn.com/2017-08-26-Markdown-Advance-Video.mp4")!)
-        presentMoviePlayerViewControllerAnimated(playerVC)
+        present(playerVC, animated: true, completion: nil)
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return array.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: HomeViewControllerCellId)!
+        cell.selectionStyle = .none
+        cell.backgroundColor = UIColor.black
+        cell.textLabel?.text = array[indexPath.row]
+        cell.textLabel?.textColor = UIColor.white
+        return cell
+    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        switch indexPath.row {
+        case 0:
+            self.showWebVC()
+            break
+        case 1:
+            self.showPictureVC()
+            break
+        case 2:
+            self.showMusicPlayerVC()
+            break
+        case 3:
+            self.showVideoPlayerVC()
+            break
+        default:
+            //do nothing
+            break
+        }
+    }
+    
+    lazy var tableView : UITableView = {
+        let tableView : UITableView = UITableView.init(frame: self.view.bounds, style: .plain)
+        tableView.backgroundColor = UIColor.black
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(UITableViewCell.classForCoder(), forCellReuseIdentifier: HomeViewControllerCellId)
+        tableView.alwaysBounceVertical = true
+        tableView.separatorStyle = .none
+        return tableView
+    }()
+    
+    override var prefersStatusBarHidden: Bool{
+        return false
     }
 }
